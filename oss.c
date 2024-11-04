9db4b9f8349d688a45602bba01714254fae6f35f
MD SARKER <131039792+masfm8@users.noreply.github.com>
2024-11-04 02:05:02 -0600

    Update oss.c

--- oss.c
+++ oss.c
@@ -4,29 +4,30 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/msg.h>
-#include <signal.h>
-#include <stdbool.h>
-#include <time.h>
 #include <sys/wait.h>
-#include "pcb.h"
-#include "clock.h"
-#include "message.h"
-#include "scheduler.h"
+#include <time.h>
+#include <signal.h>
 
 #define SHMKEY 9876
-#define MAX_PROCESSES 20
+#define MAX_PROCESSES 10  // Set to avoid excessive resource usage
+#define TERMINATION_LIMIT 3  // 3 seconds as the termination threshold
 
-// Shared memory for the system clock
-int *shmClock;
+struct msgbuf {
+    long mtype;
+    int msg;
+};
+
+// Define message types
+#define MSG_WORKER_READY 1
+#define MSG_WORKER_BLOCKED 2
+#define MSG_WORKER_TERMINATED 3
+
 int shmid, msgid;
-PCB processTable[MAX_PROCESSES];
+int *shmClock;
+pid_t pids[MAX_PROCESSES];
+time_t startTime;
 
-// Message queue key
-key_t msgkey;
-
-// Flag to stop launching new processes after 3 seconds
-bool stopLaunchingNewProcesses = false;
-
+// Function to clean up shared memory and message queue
 void cleanup() {
     shmdt(shmClock);
     shmctl(shmid, IPC_RMID, NULL);
@@ -34,100 +35,86 @@ void cleanup() {
     printf("OSS: Cleaned up shared memory and message queue.\n");
 }
 
-void sig_handler(int signo) {
-    if (signo == SIGINT) {
-        cleanup();
-        exit(0);
-    }
+// Function to check termination condition
+int shouldTerminate() {
+    return (time(NULL) - startTime) >= TERMINATION_LIMIT;
 }
 
-int main(int argc, char *argv[]) {
-    signal(SIGINT, sig_handler);
-
-    srand(time(NULL));
-
-    // Initialize shared memory and message queue
+int main() {
+    // Set up shared memory
     shmid = shmget(SHMKEY, 2 * sizeof(int), IPC_CREAT | 0666);
     shmClock = (int *)shmat(shmid, NULL, 0);
+    shmClock[0] = 0;  // seconds
+    shmClock[1] = 0;  // nanoseconds
 
-    msgkey = ftok("oss", 65);
-    msgid = msgget(msgkey, 0666 | IPC_CREAT);
+    // Set up message queue
+    key_t msgkey = ftok("oss", 65);
+    msgid = msgget(msgkey, IPC_CREAT | 0666);
 
-    // Initialize the shared clock
-    shmClock[0] = 0;  // Seconds
-    shmClock[1] = 0;  // Nanoseconds
+    // Start timing
+    startTime = time(NULL);
 
     int numProcesses = 0;
-    int launchedProcesses = 0;
-    time_t startTime = time(NULL);
 
-    while (true) {
-        // Stop launching new processes after 3 real-life seconds
-        if (difftime(time(NULL), startTime) > 3) {
-            stopLaunchingNewProcesses = true;
-        }
+    // Launch and manage user processes
+    while (!shouldTerminate() && numProcesses < MAX_PROCESSES) {
+        // Launch user process
+        pid_t pid = fork();
+        if (pid == 0) {
+            execl("./user", "user", NULL);
+            perror("oss: execl failed");
+            exit(1);
+        } else if (pid > 0) {
+            pids[numProcesses++] = pid;
+            printf("OSS: Launched user process PID %d\n", pid);
 
-        // Launch new process if allowed
-        if (!stopLaunchingNewProcesses && numProcesses < MAX_PROCESSES && launchedProcesses < MAX_PROCESSES) {
-            int slot = findAvailableSlot();
-            if (slot != -1) {
-                pid_t childPid = fork();
-                if (childPid == 0) {  // Child process
-                    execl("./worker", "worker", NULL);
-                    perror("oss: execl failed");
-                    exit(1);
-                } else if (childPid > 0) {  // OSS process
-                    processTable[slot].occupied = 1;
-                    processTable[slot].pid = childPid;
-                    processTable[slot].startSeconds = shmClock[0];
-                    processTable[slot].startNano = shmClock[1];
-                    numProcesses++;
-                    launchedProcesses++;
-                    printf("OSS: Launched worker process PID %d\n", childPid);
-                } else {
-                    perror("oss: fork failed");
-                }
-            }
-        }
+            // Send an initial message to the process with quantum information
+            struct msgbuf msg;
+            msg.mtype = pid;
+            msg.msg = 50;  // example quantum in nanoseconds
 
-        // Schedule a process based on priority
-        int processIndex = findNextProcessToSchedule(processTable, MAX_PROCESSES);
-        if (processIndex != -1) {
-            int timeQuantum = 50;  // Maximum quantum of 50ms
-
-            struct msgbuf sndMsg;
-            sndMsg.mtype = processTable[processIndex].pid;
-            sndMsg.msg = timeQuantum;
-
-            if (msgsnd(msgid, &sndMsg, sizeof(sndMsg.msg), 0) == -1) {
+            if (msgsnd(msgid, &msg, sizeof(msg.msg), 0) == -1) {
                 perror("oss: msgsnd failed");
             }
-
-            struct msgbuf rcvMsg;
-            if (msgrcv(msgid, &rcvMsg, sizeof(rcvMsg.msg), processTable[processIndex].pid, 0) == -1) {
-                perror("oss: msgrcv failed");
-            }
-
-            int timeUsed = rcvMsg.msg;
-            incrementClock(shmClock, timeUsed);
-
-            if (rcvMsg.msg == 0) {  // Process has finished
-                printf("OSS: Worker PID %d has completed.\n", processTable[processIndex].pid);
-                processTable[processIndex].occupied = 0;
-                numProcesses--;
-            } else if (rcvMsg.msg == 1) {  // Process is blocked
-                printf("OSS: Worker PID %d is blocked.\n", processTable[processIndex].pid);
-                processTable[processIndex].status = BLOCKED;
-            }
+        } else {
+            perror("oss: fork failed");
         }
 
-        // Check if all processes are done
-        if (numProcesses == 0 && stopLaunchingNewProcesses) break;
+        // Receive messages from user processes
+        struct msgbuf rcvMsg;
+        if (msgrcv(msgid, &rcvMsg, sizeof(rcvMsg.msg), 0, IPC_NOWAIT) != -1) {
+            printf("OSS: Received time used %d ns from PID %ld\n", rcvMsg.msg, rcvMsg.mtype);
+            shmClock[1] += rcvMsg.msg;
 
-        usleep(100 * 1000);  // Simulate scheduling overhead
+            // Update shared clock and check for nanoseconds overflow
+            if (shmClock[1] >= 1000000000) {
+                shmClock[0]++;
+                shmClock[1] -= 1000000000;
+            }
+            printf("OSS: Updated clock: %ds %dns\n", shmClock[0], shmClock[1]);
+        }
+
+        usleep(100 * 1000);  // interval between child launches
     }
 
-    // Cleanup
+    // Send termination signal to all remaining user processes
+    for (int i = 0; i < numProcesses; i++) {
+        struct msgbuf msg;
+        msg.mtype = pids[i];
+        msg.msg = MSG_WORKER_TERMINATED;  // Send termination signal
+
+        if (msgsnd(msgid, &msg, sizeof(msg.msg), 0) == -1) {
+            perror("oss: msgsnd termination signal failed");
+        }
+    }
+
+    // Wait for all user processes to terminate
+    for (int i = 0; i < numProcesses; i++) {
+        waitpid(pids[i], NULL, 0);
+        printf("OSS: User process PID %d has terminated.\n", pids[i]);
+    }
+
+    // Cleanup resources
     cleanup();
     return 0;
 }
